715e1c9066b6a7e1fe3b5546f2a4c2644e5a9ee6
dodoria721 <dodoria721@naver.com>
2023-03-26 20:15:28 +0900

    백준 2675번 수정

--- 2675번(문자열 반복).py
+++ 2675번(문자열 반복).py
@@ -3,11 +3,10 @@ T = int(input())
 for i in range(T): # T = 2
     R, S = input().split() # 3 ABC
     input_x = list(S) # input = ['A', 'B', 'C']
-    P = []
-    x = ''
+    P = ''
 
     for j in range(len(input_x)): # len(input) = 3
         input_x[j] = input_x[j] * int(R)
-        x += input_x[j]
+        P += input_x[j]
 
-    print(x)
+    print(P)
