a0c7c56e83fc60691a32782359ed346cf53938ac
dodoria721 <dodoria721@naver.com>
2023-04-02 23:52:18 +0900

    백준 10102번

---
+++ 10102번(개표).py
@@ -0,0 +1,9 @@
+V = int(input())
+vote = input()
+
+if vote.count('A') == vote.count('B'):
+    print('Tie')
+elif vote.count('A') > vote.count('B'):
+    print('A')
+elif vote.count('A') < vote.count('B'):
+    print('B')
