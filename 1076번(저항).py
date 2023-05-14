18cc6e53c1e40ad2dc0658d25a1fff087a7cf29d
dodoria721 <dodoria721@naver.com>
2023-05-14 22:43:27 +0900

    백준 1076번

---
+++ 1076번(저항).py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+resistance = {'black': '0', 'brown':'1', 'red':'2', 
+              'orange':'3','yellow':'4','green':'5',
+              'blue':'6','violet':'7','grey':'8','white':'9'}
+
+data = resistance[input().strip()] + resistance[input().strip()]
+power = 10 ** int(resistance[input().strip()])
+
+print(int(data) * power)
